ad120bad1599a9cf9285f379e63d17665ce4eb16
wansboods <wansboods@163.com>
2017-12-29 00:27:02 +0800

    spring + mybatis 配置

---
+++ src/main/java/cn/wansboods/o2o/dao/AreaDao.java
@@ -0,0 +1,14 @@
+package cn.wansboods.o2o.dao;
+
+import cn.wansboods.o2o.model.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    /**
+     *
+     */
+    List<Area> queryArea();
+
+
+}
