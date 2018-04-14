857e7ecd8824b529dae0c71e198315ce45e56b0a
wansboods <wansboods@163.com>
2018-04-15 01:30:10 +0800

    店铺信息编辑Dao开发

--- src/main/java/cn/wansboods/o2o/dao/ShopDao.java
+++ src/main/java/cn/wansboods/o2o/dao/ShopDao.java
@@ -3,6 +3,14 @@ package cn.wansboods.o2o.dao;
 import cn.wansboods.o2o.entity.Shop;
 
 public interface ShopDao {
+
+    /**
+     * 通过shop id 查询店铺
+     * @param shopId
+     * @return
+     */
+    Shop queryByShopId( long shopId );
+
     /**
      * 新增店铺
      * @param shop
